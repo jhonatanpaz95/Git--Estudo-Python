9ce703f7f2780154cf2219e9c701446710bba49f
Jhonatan Paz <jhonatanpaaz@gmail.com>
2023-11-01 09:52:42 -0300

    Update Desafio-3.py

--- listas/Desafios-exercicios/Desafio-3.py
+++ listas/Desafios-exercicios/Desafio-3.py
@@ -6,8 +6,20 @@ sim
 não
 """
 
-numeros_usuario = input('digite os numeros que desaja acrescentar à lista separados por espaço: ')
-fatiamento = numeros_usuario.split()
-lista_numeros.append(fatiamento)
+while True:
+    numeros_usuario = input('Digite os números que deseja acrescentar à lista separados por espaço: ')
+    
+    if numeros_usuario.replace(' ', '').isdigit():  # Verifica se a entrada contém apenas dígitos
+        break  # Se a entrada é válida, sai do loop
+    else:
+        print("Por favor, insira apenas números válidos. Tente novamente.")
 
+fatiamento = [int(numero) for numero in numeros_usuario.split()] # converte a entrada do usuário para numeros inteiros
+lista_numeros = fatiamento
 print(lista_numeros)
+
+#Calcular a soma de todos os números na lista.
+
+for numero in lista_numeros:
+    numero += numero
+    print(numero)
