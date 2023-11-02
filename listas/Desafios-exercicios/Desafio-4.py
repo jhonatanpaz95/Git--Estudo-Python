06074fdf84ffbc2868684b5de9be6df1ec4e4f2f
Jhonatan Paz <jhonatanpaaz@gmail.com>
2023-11-02 08:57:44 -0300

    atualização

--- listas/Desafios-exercicios/Desafio-4.py
+++ listas/Desafios-exercicios/Desafio-4.py
@@ -1,11 +1,21 @@
 
 lista_de_palavras = []
 
+
 entrada_usuario = input('digite as palavras ou frases que gostaria de acrescentar a lista serparados por (,): ')
 
 lista_de_palavras = [str(nome) for nome in entrada_usuario.split(',')]
 print(lista_de_palavras)
 
-#print(entrada_usuario)
-#lista_de_palavras.append(entrada_usuario)
-#print(lista_de_palavras)
+# contar quantas palavras ou frases tem na lista. (fazer isso pelo indice)
+quantidade_frases = len(lista_de_palavras)
+print(f'Existem {quantidade_frases} frases na lista.')
+
+maior_palavra = ""
+
+for palavra in lista_de_palavras:
+
+    if len(palavra) > len(maior_palavra):
+        maior_palavra = palavra
+
+print(maior_palavra)
