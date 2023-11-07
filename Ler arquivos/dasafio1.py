92b83f3d6ee626c8d7c76923401f5ea3bc0fb288
Jhonatan Paz <jhonatanpaaz@gmail.com>
2023-11-07 09:50:20 -0300

    Update dasafio1.py

--- Ler arquivos/dasafio1.py
+++ Ler arquivos/dasafio1.py
@@ -1,5 +1,8 @@
 # LER ARQUIVOS
 
-with open(file='CHATGPT.txt', mode='r', encoding='utf-8') as arquivo:
-    arquivo.read()
-    print(arquivo)
+with open(file= r'C:\Users\Samsung\Desktop\Git- Estudo Python\Ler arquivos\CHATGPT.txt', mode='r', encoding='utf-8') as arquivo:
+    conteudo = arquivo.readline()
+    conteudo = arquivo.readline()
+    arquivo.read
+    while arquivo:
+        print(conteudo.readline())
