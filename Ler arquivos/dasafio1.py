51fd36dd0c2e1fbae5ec046910c9ba033322bfb3
Jhonatan Paz <jhonatanpaaz@gmail.com>
2023-11-07 14:05:04 -0300

    Update dasafio1.py

--- Ler arquivos/dasafio1.py
+++ Ler arquivos/dasafio1.py
@@ -1,8 +1,19 @@
 # LER ARQUIVOS
+media_idades = []
 
 with open(file= r'C:\Users\Samsung\Desktop\Git- Estudo Python\Ler arquivos\CHATGPT.txt', mode='r', encoding='utf-8') as arquivo:
-    conteudo = arquivo.readline()
-    conteudo = arquivo.readline()
-    arquivo.read
-    while arquivo:
-        print(conteudo.readline())
+    linha = arquivo.readline()
+    linha = arquivo.readline()
+    while linha:
+        linha_separada = linha.split(sep= ',')
+        idade = linha_separada[1]
+        idade = int(idade)
+        media_idades.append(idade)
+        linha = arquivo.readline()
+     
+
+print(media_idades)
+media_idades = sum(media_idades) / len(media_idades)
+print(media_idades)
+
+
